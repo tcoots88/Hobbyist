74ba5fd90e3b089ae403b349dcf16ecb2f9c1494
Jerome Joof <jeromejoof@yahoo.com>
2020-02-03 12:06:48 -0800

    created different java classes to work from

---
+++ src/main/java/com/example/hobbyist/hobbyist/config/UserDetailServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.hobbyist.hobbyist.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return applicationUserRepository.findByUsername(username);
+    }
+}
